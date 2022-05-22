8335c92de436c4a20917900a528693baf5ee2ad8
GwonilJoo <gwonil3256@gmail.com>
2022-05-22 14:02:11 +0900

    adding login and signup

--- pybo/views.py
+++ pybo/views.py
@@ -3,10 +3,16 @@ from django.utils import timezone
 from .models import Question, Answer
 from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
+    page = request.GET.get('page', '1') # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
@@ -17,6 +23,7 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     print(f'request.method: {request.method}')
     question = get_object_or_404(Question, pk=question_id)
@@ -24,6 +31,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -34,11 +42,13 @@ def answer_create(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo: index')
